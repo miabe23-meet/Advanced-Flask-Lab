0d7cb080ee5a934d45768a9841b5e6d9238e9be0
miabe23-meet <109899471+miabe23-meet@users.noreply.github.com>
2022-07-28 14:07:46 +0300

    lab finished

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request, render_template, url_for
 import random
-import requests, json
 
 app = Flask(  # Create a flask app
     __name__,
@@ -25,7 +24,7 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
+    return render_template('index.html', image_link=image_link ,user_bio=user_bio , posts=posts)
 
 
 @app.route('/about')  # '/' for the default page
